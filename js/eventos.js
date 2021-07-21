c88ff066f93ba51713f2fd8c3fc824ecac9102b8
EzequielBriones <ezebriones01@gmail.com>
2021-07-20 21:09:17 -0300

    add projects to repository

---
+++ js/eventos.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $(".img1").focusin(function () {
+    $(".img1 img").addClass("composition-img--1-active");
+  });
+  $(".img1").focusout(function () {
+    $(".img1 img").removeClass("composition-img--1-active");
+  });
+});
+
+$(document).ready(function () {
+  $(".img2").focusin(function () {
+    $(".img2 img").addClass("composition-img--2-active");
+  });
+  $(".img2").focusout(function () {
+    $(".img2 img").removeClass("composition-img--2-active");
+  });
+});
+
+$(document).ready(function () {
+  $(".img3").focusin(function () {
+    $(".img3 img").addClass("composition-img--3-active");
+  });
+  $(".img3").focusout(function () {
+    $(".img3 img").removeClass("composition-img--3-active");
+  });
+});
---
+++ js/nav.js
@@ -0,0 +1,46 @@
+const menuBtn = document.querySelector(".nav");
+const modOver = document.querySelector(".modal");
+const body = document.getElementsByTagName("body")[0];
+let menuOpen = false;
+
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    modOver.classList.add("open-modal");
+    menuOpen = true;
+    body.style.overflowY = "hidden";
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    modOver.classList.remove("open-modal");
+    body.style.overflowY = "visible";
+  }
+});
+
+$("a").click(function () {
+  $("nav").removeClass("open-modal");
+  $(".nav").removeClass("open");
+  menuOpen = false;
+});
+
+$();
+
+// $(".nav").click(function () {
+//   if (!menuOpen) {
+//     $(".nav button").addClass("open");
+//     menuOpen = true;
+//   } else {
+//     $(".nav button").removeClass("open");
+//     menuOpen = false;
+//   }
+// });
+
+// window.onload = function () {
+//   let menuBtn = document.querySelector(".nav");
+//   menuBtn.addEventListener("click", agregar_clase);
+// };
+
+// function agregar_clase() {
+//   let menuBtns = document.querySelector(".nav__button");
+//   menuBtns.classList.toggle("open");
+// }
