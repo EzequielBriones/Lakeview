c6fb1670b179be28289fbedeff62c533599e0a77
EzequielBriones <ezebriones01@gmail.com>
2021-08-01 22:20:05 -0300

    add responsive improvements

--- js/nav.js
+++ js/nav.js
@@ -1,32 +1,48 @@
-const menuBtn = document.querySelector(".nav");
-const modOver = document.querySelector(".modal");
+const checkbox = document.querySelector(".navigation__checkbox");
+const background = document.querySelector(".navigation__background");
+const links = document.querySelectorAll(".navigation__links");
 const body = document.getElementsByTagName("body")[0];
 let menuOpen = false;
 
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    modOver.classList.add("open-modal");
-    menuOpen = true;
-    body.style.overflowY = "hidden";
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-    modOver.classList.remove("open-modal");
-    body.style.overflowY = "visible";
-  }
-});
+// menuBtn.addEventListener("click", () => {
+//   if (!menuOpen) {
+//     modOver.classList.add("checked");
+//     menuOpen = true;
+//     body.style.overflowY = "hidden";
+//   } else {
+//     menuOpen = false;
+//     modOver.classList.remove("open-modal");
+//     body.style.overflowY = "visible";
+//   }
+// });
 
-$("a").click(function () {
-  $("nav").removeClass("open-modal");
-  $(".nav").removeClass("open");
-  menuOpen = false;
-});
+window.onload = function () {
+  links.style.color = "red";
+};
 
-$(".modal__btns").click(function () {
-  body.style.overflowY = "visible";
-  menuOpen = false;
-});
+// checkbox.addEventListener("click", () => {
+//   if (!menuOpen) {
+//     // background.classList.add("checked");
+//     // body.style.overflowY = "hidden";
+//     links.style.color = "red";
+//   }
+// });
+
+// $(".navigation__checkbox").click(function () {
+//   $(".navigation__item a").css("display", "inline-block");
+//   $(".navigation__background").addClass("checked");
+// });
+
+// $("a").click(function () {
+//   $("nav").removeClass("open-modal");
+//   $(".nav").removeClass("open");
+//   menuOpen = false;
+// });
+
+// $(".modal__btns").click(function () {
+//   body.style.overflowY = "visible";
+//   menuOpen = false;
+// });
 // $(".nav").click(function () {
 //   if (!menuOpen) {
 //     $(".nav button").addClass("open");
